a3703691ca7e26f4142468a27a34b4b5a6762660
Taku Kotegawa <tkotegawa@stnet.co.jp>
2024-02-22 10:52:22 +0900

    Initial commit

---
+++ src/main/java/com/example/mbgexample/MbgExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbgexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbgExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbgExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mbgexample/MbgExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbgexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbgExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
